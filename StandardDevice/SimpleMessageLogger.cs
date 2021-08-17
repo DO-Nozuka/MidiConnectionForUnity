13335ba776f1e921d991e92812541c3e7b84a4ec
Donozuka <saber-wing@e-mail.jp>
2021-08-17 11:36:49 +0900

    Add SimpleMessageLogger

--- StandardDevice/SimpleMessageLogger.cs
+++ StandardDevice/SimpleMessageLogger.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using Dono.MidiConnectionForUnity.Base;
 using Dono.MidiUtilities.Runtime;
+using System;
 
 namespace MidiConnectionForUnity.StandardDevice
 {
     public class SimpleMessageLogger : MidiInDevice
     {
         public override string DefaultDeviceName => "SimpleMessageLogger";
+
+        public override void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
         public override void OnNext(MidiMessage value)
         {
             string log = "";
             log += $"[{GetDeviceName()}]";
-            log += $"msg;{value}";
+            log += $"Message:{value}";
             Debug.Log(log);
         }
 
