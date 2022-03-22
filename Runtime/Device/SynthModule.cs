80af650f7de8c4378f4e107fe265466db702476d
onozukaplan.engineer <engineer@onozukaplan.onmicrosoft.com>
2022-03-22 13:29:22 +0900

    MoveFiles, Add SynthModule

---
+++ Runtime/Device/SynthModule.cs
@@ -0,0 +1,19 @@
+﻿using Dono.Midi;
+
+#if UNITY_EDITOR
+#endif
+
+namespace Dono.MidiConnectionForUnity
+{
+    public abstract class SynthModule : MidiDevice
+    {
+        protected MidiModule midiModule = new MidiModule();
+
+        public override void OnNext(MidiMessage value)
+        {
+            base.OnNext(value);
+
+            midiModule.RecieveMessage(value);
+        }
+    }
+}
